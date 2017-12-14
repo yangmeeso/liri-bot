829a1fa0437890261a37d4f1bb68f20c6fd2fe07
yangmeeso <yangmeeso@gmail.com>
2017-12-13 16:57:48 -0800

    adding movie-this

--- liri.js
+++ liri.js
@@ -1,14 +1,21 @@
 // var Twitter = require('twitter');
-var twitterConfig = require('./keys.js');
-console.log(twitterConfig);
+var request = require("request");
+var Twitter = require('twitter');
+var config = require('./keys.js');
 
-var commands = process.argv[2]
-var movieName = process.argv[3]
+// var commands = process.argv[2]
+var movieName = process.argv[2]
 
 var params = {screen_name: 'nodejs', count: 20};
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-console.log(queryUrl)
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=81d6b13";
+console.log(queryUrl);
+
+// request(queryUrl, function(error, response, body) {
+//     if (commands === "movie-this") {
+//         console.log(JSON.parse(body));
+//         }
+// });
 
 // client.get('statuses/user_timeline', params, function(error, tweets, response) {
 //  if (!error && commands === 'my-tweets') {
