a54d52db9c2f50cb6bf667e8a4628c5b16d15321
yangmeeso <yangmeeso@gmail.com>
2017-12-15 18:43:29 -0800

    twitter functions added

--- keys.js
+++ keys.js
@@ -1,15 +1,17 @@
 
 var accessKey = {
-    twitterKey: {
-    consumer_key: '6NO5qbF1eVdA1RrQRV7D9X3fY',
-    consumer_secret: '1sPoGD3g1PorYdRUCJNusuePHKCzMNlzvHJ8bMrOzetFGU6ULJ',
-    access_token: '938586660240945152-RI2KWGuaFQ7kPiFM00LCRt7HOrF6q1S',
-    access_token_secret: 'Lje8bMKLYiPbq8arY3bsQlKVX28BG805cY6d1rn4e0sUb'
+    twitterKey : {
+        consumer_key: 'iuaob37qnVGKiosFLZ8mbBmRg',
+        consumer_secret: 'YPofFyEVYCkpTqfiUVigRHfP0XFH9YzXL87uhAHXjhvRNSpIIT',
+        access_token_key: '938586660240945152-RI2KWGuaFQ7kPiFM00LCRt7HOrF6q1S',
+        access_token_secret: 'Lje8bMKLYiPbq8arY3bsQlKVX28BG805cY6d1rn4e0sUb'
     },
-    spotifyKey: {
+    /*spotifyKey: {
 
-    },
-    omdbKey: '81d6b13'
+    },*/
+    omdbKey : {
+        apiKey: '81d6b13'
+    }
 }
 
 module.exports = accessKey;
--- liri.js
+++ liri.js
@@ -1,60 +1,58 @@
-// var Twitter = require('twitter');
 var request = require('request');
 var Twitter = require('twitter');
 var config = require('./keys.js');
 var commands = process.argv[2];
 var movieName = process.argv[3];
-
 var params = {
-    user_id: "mesoy_liri",
-    screen_name: 'nodejs', 
+    screen_name: 'mesoy_liri', // Twitter ID
     count: 20,
     exclude_replies: true,
     tweet_mode: 'extended'
 };
 
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + config.accesKey.omdbKey;
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + config.omdbKey.apiKey;
 
-function fetchTweets () {
+function fetchTweet() {
     var client = new Twitter({
-        consumer_key: config.accessKey.twitterKey.consumer_key,
-        consumer_secret: config.accesKey.twitterKey.consumer_secret,
-        access_token_key: config.accessKey.twitterKey.access_token_key,
-        access_token_secret: config.accessKey.twitterKey.access_token_secret
+        consumer_key: config.twitterKey.consumer_key,
+        consumer_secret: config.twitterKey.consumer_secret,
+        access_token_key: config.twitterKey.access_token_key,
+        access_token_secret: config.twitterKey.access_token_secret
+    });
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (commands === "my-tweets") {
+            console.log("------------------------");
+            // console.log(tweets);
+            for (var i = 0; i < tweets.length; i++) {
+                console.log("Time: " + tweets[i].created_at + "\nTweets: " + tweets[i].full_text);
+            }
+        }
+        else if (error) {
+            console.log("fetchTweet error")
+        }
+      });
+
+};
+fetchTweet();
+
+function fetchMovie() {
+    request(queryUrl, function (error, response, body) {
+        if (commands === "movie-this") {
+            // console.log(JSON.parse(body));
+            console.log(movieName);
+            console.log("------------------------")
+            console.log("Title of Movie: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Country: " + JSON.parse(body).Language);
+            console.log("Country: " + JSON.parse(body).Plot);
+            console.log("Country: " + JSON.parse(body).Actors);        
+        }
+        else if (error) {
+            console.log("fetchMovie error");
+        }
     })
 };
-
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//  if (!error && commands === 'my-tweets') {
-//    console.log(JSON.parse(tweets));
-//  }
-// });
-
-
-
-// client.get(path, params, callback);
-
-// client.get('favorites/list', function(error, tweets, response) {
-//     if(error) throw error;
-//     console.log(tweets);  // The favorites. 
-//     console.log(response);  // Raw response object. 
-//   });
-
-request(queryUrl, function fetchMovie(error, response, body) {
-    if (commands === "movie-this") {
-        // console.log(JSON.parse(body));
-        console.log(movieName);
-        console.log("------------------- MOVIE SEARCH RESULT -------------------")
-        console.log("Title of Movie: " + JSON.parse(body).Title);
-        console.log("Release Year: " + JSON.parse(body).Year);
-        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-        console.log("Country: " + JSON.parse(body).Country);
-        console.log("Country: " + JSON.parse(body).Language);
-        console.log("Country: " + JSON.parse(body).Plot);
-        console.log("Country: " + JSON.parse(body).Actors);        
-    }
-    else {
-        console.log("error");
-    }
-});
+fetchMovie();
